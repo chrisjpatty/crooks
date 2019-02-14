ff5a13d40baef6aac58bb249293823188e370747
Christopher Patty <christopher.jon.patty@gmail.com>
2019-02-14 01:45:45 -0700

    Added new build files

---
+++ dist/crooks.esm.js
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react';
+import ls from 'local-storage';
+import shortid from 'shortid';
+
+const getCache = (key, initial) => {
+  const cached = ls.get(key);
+  return cached !== null ? cached : initial
+};
+
+const useLocalStorage = (key, initial) => {
+  const [nativeState, setNativeState] = useState(getCache(key, initial));
+  const setState = state => {
+    if(typeof state === 'function'){
+      setNativeState(prev => {
+        const newState = state(prev);
+        ls.set(key, newState);
+        return newState
+      });
+    }else{
+      ls.set(key, state);
+      setNativeState(state);
+    }
+  };
+
+  return [nativeState, setState]
+};
+
+const useFiler = key => {
+  const [files, setFiles] = useLocalStorage$1(key, {});
+
+  const add = (data, id) => {
+    const newKey = id || shortid.generate();
+    const now = Date.now();
+    setFiles(files => ({
+      ...files,
+      [newKey]: {
+        id: newKey,
+        created: now,
+        modified: now,
+        data
+      }
+    }));
+    return newKey
+  };
+
+  const remove = id => {
+    setFiles(({[id]: deleted, newFiles}) => newFiles);
+  };
+
+  const update = (id, data) => {
+    setFiles(files => ({
+      ...files,
+      [id]: {
+        ...files[id],
+        modified: Date.now(),
+        data
+      }
+    }));
+  };
+
+  const clear = () => {
+    setFiles({});
+  };
+
+  return [files, {add, remove, update, clear}]
+};
+
+var keyboardShortcut = ({keyCode, action, disabled}) => {
+  useEffect(() => {
+    if(!disabled){
+      enable();
+    }
+    return () => {
+      disable();
+    }
+  });
+
+  const enable = () => {
+    document.addEventListener('keydown', handleAction);
+  };
+
+  const disable = () => {
+    document.removeEventListener('keydown', handleAction);
+  };
+
+  const handleAction = e => {
+    if(e.keyCode === keyCode){
+      e.preventDefault();
+      action(e);
+    }
+  };
+
+  return {enable, disable}
+};
+
+const useLocalStorage$1 = useLocalStorage;
+const useFiler$1 = useFiler;
+const useKeyboardShortcut = keyboardShortcut;
+
+export { useLocalStorage$1 as useLocalStorage, useFiler$1 as useFiler, useKeyboardShortcut };
